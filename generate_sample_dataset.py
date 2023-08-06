9b8eb52fb97d5bd1ddaf121751f95e8002100637
Hugo Hadfield <hadfield.hugo@gmail.com>
2023-08-06 21:07:22 +0100

    add colored tip

--- generate_sample_dataset.py
+++ generate_sample_dataset.py
@@ -6,6 +6,7 @@ and write the corresponding targets to a csv file.
 """
 
 from typing import Tuple
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
@@ -22,11 +23,17 @@ def synthesise_image(
     The vector is drawn in white and the background is black.
     """
     # Create the image
-    image = np.zeros(image_size)
+    image = np.zeros((image_size[0], image_size[1], 3), dtype=np.uint8)
 
     # Draw the vector
+    tip = (int(origin[0] + vec[0]), int(origin[1] + vec[1]))
+    tip_color = (0, 0, 255)
+    line_color = (255, 255, 255)
     image = cv2.arrowedLine(
-        image, origin, (int(origin[0] + vec[0]), int(origin[1] + vec[1])), (255, 255, 255), 2
+        image, origin, tip, tip_color, 2
+    )
+    image = cv2.line(
+        image, origin, tip, line_color, 2
     )
     return image
     
@@ -64,6 +71,7 @@ def synthesise_dataset(
 
         # Save the image
         dataset_filename = f'example_dataset/{subfolder_name}/images/image_{i}.png'
+        Path(dataset_filename).parent.mkdir(parents=True, exist_ok=True)
         cv2.imwrite(dataset_filename, image)
 
         # Add the target to the dataframe
